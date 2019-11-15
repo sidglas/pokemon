4cfb37dd957f7f789b7e790b0ca3dfb7ac294af9
sidglas <sidglas@hotmail.com>
2019-11-15 14:31:07 -0300

    fase 01

--- src/pages/Home/Pokemons.js
+++ src/pages/Home/Pokemons.js
@@ -9,9 +9,57 @@ const Pokemons = () => {
 
   const[data, setData] = useState({})    
   const[resul, setResul] = useState('')
-  const[contaPoke, setContaPoke] = useState(0)  
+  const[contaPoke, setContaPoke] = useState(20)  
+  //const[whichPoke, setWhichPoke] = useState(0)  
   const[urlDestino,setUrlDestino]= useState('https://pokeapi.co/api/v2/pokemon') 
-  
+  const[urlPokemon,setUrlPokemon]= useState('') 
+
+  useEffect(() => {
+
+  const loadPoke = async () => {
+    const res = await axios.get(urlDestino)
+    setResul(res)
+    console.log('res . data ', res.data)
+    console.log('RES RES RES', res)
+    
+    }
+
+    loadPoke()
+    
+ // console.log('espero ter feito parte 01 da carga ', resul )
+  for (let jjj = 1; jjj < 4 ; jjj++) {
+    console.log('testando rapidamente' + jjj)
+  }
+
+   }, [urlDestino])
+
+
+/* foi passada para dentro do useEffect
+
+    const loadPokeDetails = async () => {
+      resul.data.results.map((newLink) => {
+         return newLink.url = 'aaaa' + newLink.url + 'bbbb'
+      })
+    }
+    loadPokeDetails()
+ */
+/*
+  const loadPokeDetails = async () => {
+    const detalhe = await axios.get(urlPokemon)
+    await sleep(3000)
+    console.log (detalhe)
+  }
+*/
+ 
+
+ const numbers = [1, 2, 3, 4, 5];
+ const listItems = numbers.map ( (number) => { return   number * 3})
+ //const pegaVolta = (resul.data.results).map ((voltou) => { return voltou})
+
+ const whatPoke = (contaPoke, pokemonId) => {
+   return( contaPoke + parseInt(pokemonId) -19)
+ }
+ const sleep = time => new Promise(resolve => setTimeout(resolve,time))
 
   const showNext = () => {
     setContaPoke(contaPoke + 20)
@@ -21,24 +69,28 @@ const Pokemons = () => {
     setUrlDestino(baseUrl+'?'+offset)
   }
 
-  useEffect(() => {
-    const loadPoke = async () => {
-    const res = await axios.get(urlDestino)
-    setResul(res)
-    console.log('res . data ', res.data)
-    console.log('RES RES RES', res)
-    }
-  
-    loadPoke()
-  }, [urlDestino])
 
   if (resul) {
+/*
+  
+    Object
+    .keys(resul.data.results)
+    .map(pokemonId => {
+      setUrlPokemon(resul.data.results[pokemonId].url)
+      return (
+      //setUrlPokemon(resul.data.results[pokemonId].url)
+      loadPokeDetails(resul.data.results[pokemonId].url)
+      
+     )
+    })
+  */  
+
   //if (Object.keys(data.data).length > 0) {
   //if (data.data) {
-    console.log('Tamanho ' , resul.data.results.length)
-    console.log('Primeiro ' , resul.data.results[0].name)
+
     return (
       <>
+      <div className ='container'>
       <table className='table'>
       <thead>
         <tr> 
@@ -57,14 +109,41 @@ const Pokemons = () => {
                   <td> {pokemonId}</td>
                   <td>{resul.data.results[pokemonId].name}</td>
                   <td> <Link to={`https://resul.data.results[pokemonId].url`}>{resul.data.results[pokemonId].url}</Link></td>
+                  <td><img src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/shiny/${whatPoke(contaPoke, pokemonId)}.png`} className="card-img-top" alt="..."/></td>
+                  <td><button onClick={whatPoke(contaPoke, pokemonId)} className='btn btn-warning'>Consultar</button></td>
+                  <td>{whatPoke(contaPoke, pokemonId)}</td>
                 </tr>                    
               )
             })
           }
         </tbody>
       </table>
+
       <button className='btn btn-success' onClick={showNext}>Proximos</button>
+      </div>      
       <p> {contaPoke} </p>
+      <p> {listItems}</p>
+      <p> pegaVolta</p>
+      {
+        Object
+        .keys(resul.data.results)
+        .map(pokemonId => {
+          return (
+
+            <div className='card' style={{width:'14rem', display:'inlineBlock'}} key={pokemonId}>
+            <img src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/shiny/${parseInt(pokemonId)+1}.png`} className="card-img-top" alt="..."/>
+            <div className="card-body">
+              <h5 className="card-title">{resul.data.results[pokemonId].name}</h5>
+              <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              <a href="#" className="btn btn-primary">More Information</a>
+            </div>
+          </div>
+          
+
+    
+          )
+        })
+      }      
 
       </>
     )
